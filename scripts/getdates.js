dc3e3c48967206c6a34f3141f441c327ae2896d9
teory01 <youremail@byupathway.edu>
2025-05-17 15:35:55 +0100

    move

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
- // Get current year
-        document.getElementById('currentyear').textContent = new Date().getFullYear();
-        
-        // Get last modified date
-        document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
-    
+// Display current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
